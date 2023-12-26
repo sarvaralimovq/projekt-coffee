474b3dfbfbe18b0811e42881c747fe613d78980d
sarvaralimovq <sarvaralimovq@gmail.com>
2023-12-26 21:59:31 +0500

    discover tugadi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.scss';
+import Discover from './components/Discover/Discover';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
      <Header/>
      <Hero/>
+     <Discover/>
     </div>
   );
 }
