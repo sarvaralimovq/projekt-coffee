a46c281b026877687547d30c8a53faa521ef1eab
sarvaralimovq <sarvaralimovq@gmail.com>
2023-12-26 17:21:11 +0500

    salom

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import './Hero.scss'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className='Hero'>
+      <div className="container">
+        <div className="hero__inner">
+          <div className="hero__left">
+            <div className="hero__left-top">
+              <h4>We’ve got your morning covered with</h4>
+            </div>
+            <div className="hero__left-center">
+              <h1 >Coffee</h1>
+
+            </div>
+            <div className="hero__left-bottom">
+              <p>It is best to start your day with a cup of coffee. Discover the
+                best flavours coffee you will ever have. We provide the best
+                for our customers.
+              </p>
+            </div>
+            <div className="hero__left-button">
+              <button>
+                  Order Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
